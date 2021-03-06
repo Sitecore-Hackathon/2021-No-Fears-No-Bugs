603e6f7edb167c59297571bee085fca5ff1d482f
Daniel Pastor <daniel.pastor@verndale.com>
2021-03-06 10:56:56 -0500

    Map Picker V1

--- src/Feature/Controls/website/ToyBox.Feature.Controls/Models/Fields/LocationPickerViewModel.cs
+++ src/Feature/Controls/website/ToyBox.Feature.Controls/Models/Fields/LocationPickerViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Web;
 using Sitecore;
@@ -13,18 +14,23 @@ namespace ToyBox.Feature.Controls.Models.Fields
     [Serializable]
     public class LocationPickerViewModel : InputViewModel<string>
     {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
         protected override void InitItemProperties(Item item)
         {
             Assert.ArgumentNotNull((object)item, nameof(item));
             base.InitItemProperties(item);
-            this.Value = StringUtil.GetString((object) item.Fields["Default Value"]);
+            Width = MainUtil.GetInt(item.Fields["Width"]?.Value, 0);
+            Height = MainUtil.GetInt(item.Fields["Height"]?.Value, 0);
         }
 
         protected override void UpdateItemFields(Item item)
         {
             Assert.ArgumentNotNull((object)item, nameof(item));
             base.UpdateItemFields(item);
-            item.Fields["Default Value"]?.SetValue(this.Value,false);
+            item.Fields["Width"]?.SetValue(Width.ToString(CultureInfo.InvariantCulture), true);
+            item.Fields["Height"]?.SetValue(Height.ToString(CultureInfo.InvariantCulture), true);
         }
 
         protected override void InitializeValue(object value)
