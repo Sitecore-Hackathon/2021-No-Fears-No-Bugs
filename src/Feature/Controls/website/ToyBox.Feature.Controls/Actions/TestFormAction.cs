900a4bf995a6b2a70f6f8e1294f321fa6afb2bc3
Roberto Armas <roberto.armas@verndale.com>
2021-03-06 14:20:17 -0500

    Add Test Action

---
+++ src/Feature/Controls/website/ToyBox.Feature.Controls/Actions/TestFormAction.cs
@@ -0,0 +1,40 @@
+﻿using Sitecore.ExperienceForms.Models;
+using Sitecore.ExperienceForms.Processing;
+using Sitecore.ExperienceForms.Processing.Actions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+
+namespace ToyBox.Feature.Controls.Actions
+{
+    public class TestFormAction : SubmitActionBase<string>
+    {
+        public TestFormAction(ISubmitActionData submitActionData) : base(submitActionData) 
+        {
+                
+        }
+
+        /// <summary>
+        /// Tries to convert the specified <paramref name="value" /> to an instance of the specified target type.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <param name="target">The target object.</param>
+        /// <returns>
+        /// true if <paramref name="value" /> was converted successfully; otherwise, false.
+        /// </returns>
+        protected override bool TryParse(string value, out string target)
+        {
+            target = string.Empty;
+            return true;
+        }
+
+        protected override bool Execute(string data, FormSubmitContext formSubmitContext)
+        {
+            
+            return true;
+        }
+    }
+}
